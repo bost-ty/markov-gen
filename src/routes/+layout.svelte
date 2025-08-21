d4489522570c83ad3b86b089cd8e09d93ca5c069
Will <61333914+bost-ty@users.noreply.github.com>
2025-08-20 21:02:58 -0700

    feat(layout): add link to bostwick.dev

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,4 +7,10 @@
 	let { children } = $props();
 </script>
 
+<div class="absolute top-1 right-1">
+	<a class="text-sky-700 hover:text-sky-800"
+	   href="https://bostwick.dev"
+	>see more @ bostwick.dev</a>
+</div>
+
 {@render children()}
